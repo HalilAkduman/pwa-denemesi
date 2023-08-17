26616175ead19e78fd0e0aec90a3faa67363d674
Halil Akduman <halil@hemosoft.com>
2023-08-17 21:51:51 +0300

    ilk commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: 'ana-sayfa', pathMatch: 'full' },
+  { path: 'ana-sayfa', component: HomeComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
